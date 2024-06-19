967f669d35ea4b77b031d2c20f7f6b1951c2abad
MikhailStukalau <m.stukalau@gmail.com>
2024-06-19 19:56:33 +0300

    upd: add method delete

--- app/service/base.py
+++ app/service/base.py
@@ -1,5 +1,5 @@
 from app.database import async_session_maker
-from sqlalchemy import select, insert
+from sqlalchemy import select, insert, delete
 
 
 class BaseService:
@@ -33,5 +33,12 @@ class BaseService:
     async def add(cls, **data):
         async with async_session_maker() as session:
             query = insert(cls.model).values(**data)
+            await session.execute(query)
+            await session.commit()
+
+    @classmethod
+    async def delete(cls, **filter_by):
+        async with async_session_maker() as session:
+            query = delete(cls.model).filter_by(**filter_by)
             await session.execute(query)
             await session.commit()
