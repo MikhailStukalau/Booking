a220e1b9a4ed066a3409dc8057b199acbf709723
MikhailStukalau <m.stukalau@gmail.com>
2024-06-25 01:38:07 +0300

    add: image compression

--- app/images/router.py
+++ app/images/router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, UploadFile
+from app.tasks.tasks import process_pic
 import shutil
 
 router = APIRouter(
@@ -9,5 +10,8 @@ router = APIRouter(
 
 @router.post("/")
 async def add_hotel_image(name: int, file: UploadFile):
-    with open(f"app/static/images/{name}.webp", "wb+") as file_object:
+    im_path = f"app/static/images/{name}.webp"
+    with open(im_path, "wb+") as file_object:
         shutil.copyfileobj(file.file, file_object)
+    process_pic.delay(im_path)
+
