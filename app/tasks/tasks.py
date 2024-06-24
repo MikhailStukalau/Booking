82538be96cdda6ae72969978b98f0757a4d07099
MikhailStukalau <m.stukalau@gmail.com>
2024-06-25 02:30:37 +0300

    add: tasks pic and email confirm

---
+++ app/tasks/tasks.py
@@ -0,0 +1,32 @@
+from app.tasks.celery import celery
+from PIL import Image
+from pathlib import Path
+from pydantic import EmailStr
+from app.config import settings
+from app.tasks.email_templates import create_booking_confirmation_template
+import smtplib
+
+
+@celery.task
+def process_pic(
+        path: str,
+):
+    im_path = Path(path)
+    im = Image.open(im_path)
+    im_resized_1000_500 = im.resize((1000, 500))
+    im_resized_200_100 = im.resize((200, 100))
+    im_resized_1000_500.save(f"app/static/images/resized_1000_500{im_path.name}")
+    im_resized_200_100.save(f"app/static/images/resized_200_100{im_path.name}")
+
+
+@celery.task
+def send_booking_confirmation_email(
+        booking: dict,
+        email_to: EmailStr,
+):
+    email_to_mock = settings.SMTP_USER
+    msg_content = create_booking_confirmation_template(booking, email_to_mock)
+
+    with smtplib.SMTP_SSL(settings.SMTP_HOST, settings.SMTP_PORT) as server:
+        server.login(settings.SMTP_USER, settings.SMTP_PASS)
+        server.send_message(msg_content)
