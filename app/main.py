ddd29c5f300fcf77d513a3bb8bdf0860f930ca29
MikhailStukalau <m.stukalau@gmail.com>
2024-06-27 14:43:12 +0300

    Add: Admin

--- app/main.py
+++ app/main.py
@@ -1,17 +1,66 @@
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from sqladmin import Admin
 
 from app.bookings.router import router as router_bookings
 from app.users.router import router_users, router_auth
 from app.hotels.router import router as router_hotels
+from app.pages.router import router as router_pages
+from app.images.router import router as router_images
+from app.config import settings
+from app.database import engine
+from app.admin.views import UsersAdmin, BookingsAdmin, HotelsAdmin, RoomsAdmin
+
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from fastapi_cache.decorator import cache
+
+from redis import asyncio as aioredis
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    redis = aioredis.from_url(f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}",
+                              encoding="utf8", decode_responses=True)
+    FastAPICache.init(RedisBackend(redis), prefix="cache")
+    yield
+
+
+app = FastAPI(
+    lifespan=lifespan
+)
+
+app.mount("/static", StaticFiles(directory="app/static"), "static")  # монтирование статичных файлов
 
 app.include_router(router_auth)
 app.include_router(router_users)
 app.include_router(router_hotels)
 app.include_router(router_bookings)
+app.include_router(router_pages)
+app.include_router(router_images)
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
+admin = Admin(app, engine)
+
+admin.add_view(UsersAdmin)
+admin.add_view(BookingsAdmin)
+admin.add_view(HotelsAdmin)
+admin.add_view(RoomsAdmin)
 
 
 
